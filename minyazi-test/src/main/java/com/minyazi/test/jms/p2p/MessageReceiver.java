565744ddd8cb5968acdcb4497f57c8e601c226e7
minyazi <minyazi@yeah.net>
2017-09-19 00:38:40 +0800

    Update JMS and ActiveMQ P2P test program.

--- minyazi-test/src/main/java/com/minyazi/test/jms/p2p/MessageReceiver.java
+++ minyazi-test/src/main/java/com/minyazi/test/jms/p2p/MessageReceiver.java
@@ -1,11 +1,11 @@
 package com.minyazi.test.jms.p2p;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.JMSException;
-import javax.jms.Message;
 import javax.jms.MessageConsumer;
-import javax.jms.MessageListener;
 import javax.jms.Queue;
 import javax.jms.Session;
 import javax.jms.TextMessage;
@@ -19,16 +19,17 @@ import org.apache.activemq.ActiveMQConnectionFactory;
  * @author minyazi
  */
 public class MessageReceiver {
-    private static final String USERNAME = ActiveMQConnection.DEFAULT_USER; // 默认的连接用户名
-    private static final String PASSWORD = ActiveMQConnection.DEFAULT_PASSWORD; // 默认的连接密码
-    private static final String BROKERURL = ActiveMQConnection.DEFAULT_BROKER_URL; // 默认的连接地址
+    private static final String USERNAME = ActiveMQConnection.DEFAULT_USER; // ActiveMQ的默认用户名
+    private static final String PASSWORD = ActiveMQConnection.DEFAULT_PASSWORD; // ActiveMQ的默认密码
+    private static final String BROKERURL = ActiveMQConnection.DEFAULT_BROKER_URL; // ActiveMQ的默认连接地址
     
-    public static void main(String[] args) {
-        ConnectionFactory connectionFactory = null; // 连接工厂
-        Connection connection = null; // 连接
-        Session session = null; // 会话
-        Queue queue = null; // 消息队列
-        MessageConsumer messageConsumer = null; // 消息消费者
+    private AtomicInteger counter = new AtomicInteger(0); // 计数器
+    private ConnectionFactory connectionFactory; // 连接工厂
+    private Connection connection; // 连接
+    private Session session; // 会话
+    private ThreadLocal<MessageConsumer> threadLocal = new ThreadLocal<MessageConsumer>();
+    
+    public MessageReceiver() {
         try {
             // 实例化连接工厂
             connectionFactory = new ActiveMQConnectionFactory(USERNAME, PASSWORD, BROKERURL);
@@ -38,53 +39,100 @@ public class MessageReceiver {
             connection.start();
             // 创建会话
             session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void receiveMessage(String queueName) {
+        Queue queue = null; // 消息队列
+        MessageConsumer messageConsumer = null; // 消息消费者
+        try {
             // 创建消息队列
             queue = session.createQueue("MESG_QUEUE");
-            // 创建消息消费者
-            messageConsumer = session.createConsumer(queue);
+            if (threadLocal.get() != null) {
+                messageConsumer = threadLocal.get();
+            } else {
+                // 创建消息消费者
+                messageConsumer = session.createConsumer(queue);
+                threadLocal.set(messageConsumer);
+            }
             
-            /*
             while (true) {
+                Thread.sleep(1000);
                 // 接收消息
-                TextMessage textmessage = (TextMessage) messageConsumer.receive(10000);
-                if (textmessage != null) {
-                    System.out.println("接收消息：" + textmessage.getText());
+                TextMessage mesg = (TextMessage) messageConsumer.receive();
+                if (mesg != null) {
+                    String message = Thread.currentThread().getName() + " -> 接收消息：" + mesg.getText() + "，计数：" + counter.getAndIncrement();
+                    System.out.println(message);
                 } else {
-                    System.out.println("队列中没有消息");
                     break;
                 }
             }
-            */
             
+            /*
             // 注册消息监听器
             messageConsumer.setMessageListener(new MessageListener() {
                 @Override
-                public void onMessage(Message message) {
+                public void onMessage(Message mesg) {
                     try {
-                        System.out.println("接收消息：" + ((TextMessage) message).getText());
+                        String message = Thread.currentThread().getName() + " -> 接收消息：" + ((TextMessage) mesg).getText() + "，计数：" + counter.getAndIncrement();
+                        System.out.println(message);
                     } catch (JMSException e) {
                         e.printStackTrace();
                     }
                 }
             });
+            */
         } catch (JMSException e) {
             e.printStackTrace();
-        } finally {
-            /*
-            try {
-                if (messageConsumer != null) {
-                    messageConsumer.close();
-                }
-                if (session != null) {
-                    session.close();
-                }
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch (JMSException e) {
-                e.printStackTrace();
-            }
-            */
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+    }
+    
+    public void close() {
+        try {
+            if (threadLocal.get() != null) {
+                threadLocal.get().close();
+            }
+            if (session != null) {
+                session.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private class MessageReceiverThread implements Runnable {
+        private MessageReceiver messageReceiver;
+        
+        public MessageReceiverThread(MessageReceiver messageReceiver) {
+            this.messageReceiver = messageReceiver;
+        }
+        
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    messageReceiver.receiveMessage("MESG_QUEUE");
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        MessageReceiver messageReceiver = new MessageReceiver();
+        new Thread(messageReceiver.new MessageReceiverThread(messageReceiver)).start();
+        new Thread(messageReceiver.new MessageReceiverThread(messageReceiver)).start();
+        new Thread(messageReceiver.new MessageReceiverThread(messageReceiver)).start();
+        new Thread(messageReceiver.new MessageReceiverThread(messageReceiver)).start();
+        new Thread(messageReceiver.new MessageReceiverThread(messageReceiver)).start();
     }
 }
--- minyazi-test/src/main/java/com/minyazi/test/jms/p2p/MessageSender.java
+++ minyazi-test/src/main/java/com/minyazi/test/jms/p2p/MessageSender.java
@@ -1,5 +1,7 @@
 package com.minyazi.test.jms.p2p;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.JMSException;
@@ -17,16 +19,17 @@ import org.apache.activemq.ActiveMQConnectionFactory;
  * @author minyazi
  */
 public class MessageSender {
-    private static final String USERNAME = ActiveMQConnection.DEFAULT_USER; // 默认的连接用户名
-    private static final String PASSWORD = ActiveMQConnection.DEFAULT_PASSWORD; // 默认的连接密码
-    private static final String BROKERURL = ActiveMQConnection.DEFAULT_BROKER_URL; // 默认的连接地址
+    private static final String USERNAME = ActiveMQConnection.DEFAULT_USER; // ActiveMQ的默认用户名
+    private static final String PASSWORD = ActiveMQConnection.DEFAULT_PASSWORD; // ActiveMQ的默认密码
+    private static final String BROKERURL = ActiveMQConnection.DEFAULT_BROKER_URL; // ActiveMQ的默认连接地址
     
-    public static void main(String[] args) {
-        ConnectionFactory connectionFactory = null; // 连接工厂
-        Connection connection = null; // 连接
-        Session session = null; // 会话
-        Queue queue = null; // 消息队列
-        MessageProducer messageProducer = null; // 消息生产者
+    private AtomicInteger counter = new AtomicInteger(0); // 计数器
+    private ConnectionFactory connectionFactory; // 连接工厂
+    private Connection connection; // 连接
+    private Session session; // 会话
+    private ThreadLocal<MessageProducer> threadLocal = new ThreadLocal<MessageProducer>();
+    
+    public MessageSender() {
         try {
             // 实例化连接工厂
             connectionFactory = new ActiveMQConnectionFactory(USERNAME, PASSWORD, BROKERURL);
@@ -35,34 +38,86 @@ public class MessageSender {
             // 启动连接
             connection.start();
             // 创建会话
-            session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
-            // 创建消息队列
-            queue = session.createQueue("MESG_QUEUE");
-            // 创建消息生产者
-            messageProducer = session.createProducer(queue);
-            // 创建消息
-            TextMessage textmessage = session.createTextMessage("Hello World");
-            System.out.println("发送消息：Hello World");
-            // 发送消息
-            messageProducer.send(textmessage);
-            // 提交事务
-            session.commit();
+            session = connection.createSession(true, Session.SESSION_TRANSACTED);
         } catch (JMSException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                if (messageProducer != null) {
-                    messageProducer.close();
+        }
+    }
+    
+    public void sendMessage(String queueName) {
+        Queue queue = null; // 消息队列
+        MessageProducer messageProducer = null; // 消息生产者
+        try {
+            // 创建消息队列
+            queue = session.createQueue(queueName);
+            if (threadLocal.get() != null) {
+                messageProducer = threadLocal.get();
+            } else {
+                // 创建消息生产者
+                messageProducer = session.createProducer(queue);
+                threadLocal.set(messageProducer);
+            }
+            
+            while (true) {
+                Thread.sleep(1000);
+                // 创建消息
+                String message = Thread.currentThread().getName() + " -> 发送消息：Hello World，计数：" + counter.getAndIncrement();
+                System.out.println(message);
+                TextMessage mesg = session.createTextMessage(message);
+                // 发送消息
+                messageProducer.send(mesg);
+                // 提交事务
+                session.commit();
+            }
+        } catch (JMSException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void close() {
+        try {
+            if (threadLocal.get() != null) {
+                threadLocal.get().close();
+            }
+            if (session != null) {
+                session.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private class MessageSenderThread implements Runnable {
+        private MessageSender messageSender;
+        
+        public MessageSenderThread(MessageSender messageSender) {
+            this.messageSender = messageSender;
+        }
+        
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    messageSender.sendMessage("MESG_QUEUE");
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-                if (session != null) {
-                    session.close();
-                }
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch (JMSException e) {
-                e.printStackTrace();
             }
         }
+    }
+    
+    public static void main(String[] args) {
+        MessageSender messageSender = new MessageSender();
+        new Thread(messageSender.new MessageSenderThread(messageSender)).start();
+        new Thread(messageSender.new MessageSenderThread(messageSender)).start();
+        new Thread(messageSender.new MessageSenderThread(messageSender)).start();
+        new Thread(messageSender.new MessageSenderThread(messageSender)).start();
+        new Thread(messageSender.new MessageSenderThread(messageSender)).start();
     }
 }
